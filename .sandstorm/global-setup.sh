a43777834b4afe76227980ac5a1c1c898a69737b
JJ <jan@jambor.pro>
2015-05-08 19:31:36 +0200

    VM can be accessed now
    Starting of the app via ```vagrant-spk dev``` is not yet working as the browser wants to download a file instead of displaying it.

--- .sandstorm/global-setup.sh
+++ .sandstorm/global-setup.sh
@@ -2,15 +2,27 @@
 set -eu
 echo localhost > /etc/hostname
 hostname localhost
-curl https://install.sandstorm.io/ > install.sh
-bash install.sh -d -e
+curl https://install.sandstorm.io/ > /host-dot-sandstorm/caches/install.sh
+SANDSTORM_CURRENT_VERSION=$(curl -fs "https://install.sandstorm.io/dev?from=0&type=install")
+SANDSTORM_PACKAGE="sandstorm-$SANDSTORM_CURRENT_VERSION.tar.xz"
+if [[ ! -f /host-dot-sandstorm/caches/$SANDSTORM_PACKAGE ]] ; then
+    curl --output "/host-dot-sandstorm/caches/$SANDSTORM_PACKAGE" "https://dl.sandstorm.io/$SANDSTORM_PACKAGE"
+fi
+bash /host-dot-sandstorm/caches/install.sh -d -e "/host-dot-sandstorm/caches/$SANDSTORM_PACKAGE"
 modprobe ip_tables
 # Make the vagrant user part of the sandstorm group so that commands like
 # `spk dev` work.
 usermod -a -G 'sandstorm' 'vagrant'
+# Bind to all addresses, so the vagrant port-forward works.
+sudo sed --in-place='' \
+        --expression='s/^BIND_IP=.*/BIND_IP=0.0.0.0/' \
+        /opt/sandstorm/sandstorm.conf
 # TODO: update sandstorm installer script to ask about dev accounts, and
 # specify a value for this option in the default config?
 if ! grep --quiet --no-messages ALLOW_DEV_ACCOUNTS=true /opt/sandstorm/sandstorm.conf ; then
     echo "ALLOW_DEV_ACCOUNTS=true" | sudo tee -a /opt/sandstorm/sandstorm.conf
     sudo service sandstorm restart
 fi
+# stop some other services that are running by default in vagrant VMs
+sudo service puppet stop
+sudo service chef-client stop
--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y nginx php5-fpm php5-mysql mariadb-server
 apt-get install -y php5-gd 
 apt-get install -y php5-mcrypt
-#sudo php5enmod mcrypt
+php5enmod mcrypt
 sudo unlink /etc/nginx/sites-enabled/default
 cat > /etc/nginx/sites-available/sandstorm-php <<EOF
 upstream php-handler {
@@ -16,27 +16,30 @@ server {
 
   server_name localhost;
   root /opt/app/frontend/public;
-  
+
   include mime.types;
   default_type application/octet-stream;
   sendfile on;
   keepalive_timeout 65;
   client_max_body_size 1000M;
+
   location / {
     index index.html index.htm index.php;
     try_files $uri $uri/ /index.php;
   }
-  location ~ \.php$ {
+  location ~ \\.php\$ {
+    fastcgi_split_path_info ^(.+\\.php)(/.+)\$;
+    fastcgi_pass unix:/var/run/php5-fpm.sock;
     try_files $uri = 404;
     fastcgi_index index.php;
     include fastcgi_params;
-  } 
+  }
 }
 EOF
-sudo ln -s /etc/nginx/sites-available/sandstorm-php /etc/nginx/sites-enabled/sandstorm-php
-sudo service nginx stop
-sudo service php5-fpm stop
-sudo service mysql stop
+ln -s /etc/nginx/sites-available/sandstorm-php /etc/nginx/sites-enabled/sandstorm-php
+service nginx stop
+service php5-fpm stop
+service mysql stop
 # patch /etc/php5/fpm/php-fpm.conf to not change uid/gid to www-data
 sed --in-place='' \
         --expression='s/^listen.owner = www-data/#listen.owner = www-data/' \
@@ -48,19 +51,28 @@ sed --in-place='' \
 sed --in-place='' \
         --expression='s/^user\t\t= mysql/#user\t\t= mysql/' \
         /etc/mysql/my.cnf
-
 ## minimize mysql allocations
 # echo '[mysqld]\ninnodb_data_file_path = ibdata1:10M:autoextend\ninnodb_log_file_size = 10KB\ninnodb_file_per_table = 1' > /etc/mysql/conf.d/small.cnf
 # sed -i 's_^socket\s*=.*_socket = /tmp/mysqld.sock_g' /etc/mysql/*.cnf && ln -s /tmp/mysqld.sock /var/run/mysqld/mysqld.sock
 # rm -rf /var/lib/mysql/* && mysql_install_db && chown -R mysql: /var/lib/mysql
-
 ## initial paperwork DB
 # /usr/sbin/mysqld & \
 # sleep 10s &&\
 # echo "DROP DATABASE IF EXISTS paperwork; CREATE DATABASE IF NOT EXISTS paperwork DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; GRANT ALL PRIVILEGES ON paperwork.* TO 'paperwork'@'localhost' IDENTIFIED BY 'paperwork' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql
-
-## patch nginx conf to not bother trying to setuid, since we're not root
+# patch nginx conf to not bother trying to setuid, since we're not root
 sed --in-place='' \
         --expression 's/^user www-data/#user www-data/' \
         --expression 's#^pid /run/nginx.pid#pid /var/run/nginx.pid#' \
         /etc/nginx/nginx.conf
+# Add a conf snippet providing what sandstorm-http-bridge says the protocol is as var fe_https
+cat > /etc/nginx/conf.d/50sandstorm.conf << EOF
+    # Trust the sandstorm-http-bridge's X-Forwarded-Proto.
+    map \$http_x_forwarded_proto \$fe_https {
+        default "";
+        https on;
+    }
+EOF
+# Adjust fastcgi_params to use the patched fe_https
+sed --in-place='' \
+        --expression 's/^fastcgi_param\tHTTPS.*$/fastcgi_param\tHTTPS\t\t\$fe_https if_not_empty;/' \
+        /etc/nginx/fastcgi_params
