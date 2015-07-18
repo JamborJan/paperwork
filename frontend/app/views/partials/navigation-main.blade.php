ebc4cfd7880b06035061fac3e9d6ec2912393583
JJ <jan@jambor.pro>
2015-07-18 18:52:04 +0200

    Hide buttons
    ... which are not useful in Sandstorm.

--- frontend/app/views/partials/navigation-main.blade.php
+++ frontend/app/views/partials/navigation-main.blade.php
@@ -1,38 +1,43 @@
 <ul class="nav navbar-nav navbar-right">
 	@include("partials.navbar.navbar-link", [
 			"route" => URL::route("/"),
-			"title" => Lang::get("keywords.library"), 
+			"title" => Lang::get("keywords.library"),
 			"fa_icon" => "fa-book"
 		])
 
-	@include("partials.navbar.navbar-link", [
-			"route" => URL::route("user/profile"),
-			"title" => Lang::get("keywords.profile"), 
-			"fa_icon" => "fa-user"
-		])
+	@if(Config::get('paperwork.sandstorm_debug') == false)
+		@include("partials.navbar.navbar-link", [
+				"route" => URL::route("user/profile"),
+				"title" => Lang::get("keywords.profile"),
+				"fa_icon" => "fa-user"
+			])
+	@endif
 
 	@include("partials.navbar.navbar-link", [
 			"route" => URL::route("user/settings"),
-			"title" => Lang::get("keywords.settings"), 
+			"title" => Lang::get("keywords.settings"),
 			"fa_icon" => "fa-cog"
 		])
 
 	@if (Auth::user() && Auth::user()->isAdmin())
 		@include("partials.navbar.navbar-link", [
 				"route" => URL::route("admin/console"),
-				"title" => Lang::get("keywords.admin_area"), 
+				"title" => Lang::get("keywords.admin_area"),
 				"fa_icon" => "fa-star"
 			])
 	@endif
 
 	@include("partials.navbar.navbar-link", [
 			"route" => URL::route("user/help"),
-			"title" => Lang::get("keywords.help"), 
+			"title" => Lang::get("keywords.help"),
 			"fa_icon" => "fa-question"
 		])
-	@include("partials.navbar.navbar-link", [
-			"route" => URL::route("user/logout"),
-			"title" => Lang::get("keywords.sign_out"), 
-			"fa_icon" => "fa-sign-out"
-		])
+
+	@if(Config::get('paperwork.sandstorm_debug') == false)
+		@include("partials.navbar.navbar-link", [
+				"route" => URL::route("user/logout"),
+				"title" => Lang::get("keywords.sign_out"),
+				"fa_icon" => "fa-sign-out"
+			])
+	@endif
 </ul>
