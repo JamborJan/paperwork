01bd733ebda5673734b3a9546b14e658cdd0da1b
JJ <jan@jambor.pro>
2015-08-21 11:49:30 +0200

    Typos fixed

--- frontend/app/views/partials/debug-main.blade.php
+++ frontend/app/views/partials/debug-main.blade.php
@@ -1,12 +1,12 @@
 <p>
-HTTP headers:</br>
-FORWARDED_PROTO: <?php echo $_SERVER[ 'HTTP_X_FORWARDED_PROTO']; ?><br/>
-PERMISSIONS: <?php echo $_SERVER[ 'HTTP_X_SANDSTORM_PERMISSIONS']; ?><br/>
-<br/>
-Amount of users: <?php echo User::all()->count()?><br/>
-Amount of notebooks: <?php echo Notebook::all()->count()?><br/>
-Amount of tags: <?php echo Tag::all()->count()?><br/>
-Amount of notes: <?php echo Note::all()->count()?><br/>
-<br/>
-Amount of Init Steps: <?php echo DB::table('migrations')->where('batch', '=', 1)->count();?><br/>
+  HTTP headers:<br/>
+  FORWARDED_PROTO: <?php echo $_SERVER[ 'HTTP_X_FORWARDED_PROTO']; ?><br/>
+  PERMISSIONS: <?php echo $_SERVER[ 'HTTP_X_SANDSTORM_PERMISSIONS']; ?><br/>
+  <br/>
+  Amount of users: <?php echo User::all()->count()?><br/>
+  Amount of notebooks: <?php echo Notebook::all()->count()?><br/>
+  Amount of tags: <?php echo Tag::all()->count()?><br/>
+  Amount of notes: <?php echo Note::all()->count()?><br/>
+  <br/>
+  Amount of Init Steps: <?php echo DB::table('migrations')->where('batch', '=', 1)->count();?><br/>
 </p>
